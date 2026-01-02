9aaedbcadc14b7f0a0ec00e56439b82489c59411
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-03 00:44:28 +0600

    Sum of N

---
+++ sum_of_n.py
@@ -0,0 +1,11 @@
+n = int(input("Enter N for : ")) #defined N for calculate
+sum = 0 #defined a variable  for hold the sum
+i = 0 #Defined i for counting Loop
+
+while i <= n:
+    
+    sum = sum + i #Update the running total
+
+    i += 1 # increment i by 1
+
+print(sum)
