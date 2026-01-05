e45a40af17bd763d17e954db49e0d5015836789f
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-05 20:57:31 +0600

    Sum of N without lopp

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def factorial(n):
+
+    result = 1
+
+    while n > 0:
+        result *= n
+        n-= 1
+
+    return result
+
+x = factorial(5)
+print(x)
+
+
+
---
+++ sum_of_n_without_loop.py
@@ -0,0 +1,5 @@
+n = int(input("Enter N TO Get Sum From 'ZERO' : "))
+
+sum_result = sum(range(0, n+1)) # if we input 10 it will sum until 9
+                                # That why I used n+1 
+print(sum_result)
