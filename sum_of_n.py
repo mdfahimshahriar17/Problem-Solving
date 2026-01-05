bdf8539fb2a9aad458afec75be48bf33e68f220c
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-05 21:00:42 +0600

    updated sum of n

--- sum_of_n.py
+++ sum_of_n.py
@@ -9,3 +9,10 @@ while i <= n:
     i += 1 # increment i by 1
 
 print(sum)
+
+# Sum of N without loop
+n = int(input("Enter N TO Get Sum From 'ZERO' : "))
+
+sum_result = sum(range(0, n+1)) # if we input 10 it will sum until 9
+                                # That why I used n+1 
+print(sum_result)
