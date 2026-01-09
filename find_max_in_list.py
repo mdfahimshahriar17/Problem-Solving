b62b7080294ad7dbdb518b35ed1be6b2f5d0e6b0
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-10 01:02:40 +0600

    Updated score tracker, how can we doit by using dictionary

--- find_max_in_list.py
+++
@@ -1,4 +0,0 @@
-numbers = [5, 2, 9, 1, 7]
-max_val, min_val = (numbers)
-print(max_val)  # 9
-print(min_val)  # 1
--- score_tracker.py
+++ score_tracker.py
@@ -18,3 +18,32 @@ if name_and_score:
     print("NO. Name Score")
     for idx in range(0, len(name_and_score)):
         print(f"{idx+1}. {name_and_score[idx][0]} {name_and_score[idx][1]}")
+
+
+
+
+
+name_and_score = []
+
+while True:
+    student = {}
+    name = input("Enter name : ").strip()
+
+    if name.lower() == "stop":
+        break
+
+    score = int(input("Enter score : "))
+
+    student[name] = score
+    
+
+    name_and_score.append(student)
+
+print(name_and_score)
+if name_and_score:
+    count = 1
+    print("NO. Name Score")
+    for student in name_and_score:
+        for name, score in student.items():
+            print(f"{count} {name} {score}")
+            count +=1
