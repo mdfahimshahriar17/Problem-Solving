971c233757ce1af0d50533c38239b40738c80fa5
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-11 23:07:42 +0600

    Added find largestr even function

---
+++ largest_even_number.py
@@ -0,0 +1,23 @@
+def find_largest_even(numbers):
+
+    even_num = []
+    max_even_num = 0
+
+    for idx in range(0, len(numbers)):
+        if numbers[idx]%2 == 0:
+            even_num.append(numbers[idx])
+
+            for num in even_num:
+                if num > max_even_num:
+                    max_even_num = num       
+
+    if not max_even_num:
+        max_even_num = None        
+    return max_even_num
+
+           
+numbers = [3, 7, 2, 8, 5]
+print(find_largest_even(numbers))   # 8
+
+numbers = [1, 3, 5]
+print(find_largest_even(numbers))   # None
