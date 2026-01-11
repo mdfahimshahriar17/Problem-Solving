799852d5f0f19392b68c30c7fa8c19fa961dada4
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-11 22:32:46 +0600

    Updated even number method by returning a new list

--- even_number.py
+++ even_number.py
@@ -1,18 +1,26 @@
-def find_even_number():
-    number = [1,2,3,4,5,6,7,8,9,10,11,12]
-    for i in number:
-        if i%2 == 0:
-            print(i)
-
 
 def even_number():
     for i in range(1, 12+1): # I start count from 0 so in range function must have to declear starting point. range function don't count last index 
         if i%2 == 0:
             print(i)
 
+even_number()
+
+
+def get_even_numbers(numbers):
+    new_numbers = []
+
+    for num in numbers:
+        if num%2 == 0:
+
+            new_numbers.append(num)
+    return new_numbers
+            
 
 
 
 
-find_even_number()
-even_number()            
+numbers = [1, 2, 3, 4, 5, 6]
+result = get_even_numbers(numbers)
+print(result)   # [2, 4, 6]
+
