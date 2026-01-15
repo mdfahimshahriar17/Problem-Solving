d1d5d3ebb7d92d5f835ec73f4ff2fea4980a1793
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-15 22:57:20 +0600

    Updated count positive integer

--- count_positive_integer_in_list.py
+++ count_positive_integer_in_list.py
@@ -2,12 +2,11 @@ def count_positive_integer(nums):
     count = 0
 
     for num in nums:
-        if num > 0:
+        if int(num) > 0:
             count += 1
 
     return count
 
-list_of_nums = [-2, 3, 0, 7, -1]
-
-result = count_positive_integer(list_of_nums)
+nums = input("Input numbers : ").split()
+result = count_positive_integer(nums)
 print(result)
