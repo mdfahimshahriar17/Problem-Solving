a668a28adc21a3fbb28b82e811499d97edff09d1
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-17 08:34:55 +0600

    Updated Frequency Counter

--- count_frequency.py
+++ count_frequency.py
@@ -1,19 +1,15 @@
-def count_frequency(numbers):
-    
+def frequency_counter(nums):
     freq = {}
 
-    for num in numbers:
+    for num in nums:
         if num not in freq:
-            freq[num] = 1        
+            freq[num] = 1
+
         else:
-            freq[num] += 1       
+            freq[num] += 1
+
     return freq
 
-
-numbers = [1, 2, 2, 3, 1, 4]
-result = count_frequency(numbers)
+nums = [1, 2, 2, 3, 1, 4, 2]
+result = frequency_counter(nums)
 print(result)
-
-
-
-    
