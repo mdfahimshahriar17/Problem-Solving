d33dd1da33250eda583536a96786750b09d1f836
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-20 17:10:43 +0600

    Added a comment for understanding the factorial

--- factorial.py
+++ factorial.py
@@ -1,11 +1,12 @@
 def factorial(n):
-
+    #5! = 5 × 4 × 3 × 2 × 1
     result = 1
 
     while n > 0:
         result *= n
+        print(result)
         n-= 1
-
+        print(n)
     return result
 
 x = factorial(5)
