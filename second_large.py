bcaaaf4c0a82122cb69c22df5853e32b954cab7e
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-20 19:36:27 +0600

    Added diffrent way to find second large

--- second_large.py
+++ second_large.py
@@ -23,3 +23,28 @@ def find_second_largest(nums):
 nums = [55, 22, 33, 11, 88, 66, 44]
 result = find_second_largest(nums)
 print(f"Second Large -- {result[1]}\nLarge -- {result[0]}")
+
+
+
+
+
+
+
+def second_large(nums):
+    highest = None
+    second_highest = None
+
+    for num in nums:
+        if highest == None or num > highest:
+            second_highest = highest
+            highest = num
+
+        elif num < highest and num > second_highest:
+            second_highest = num
+
+    return highest, second_highest
+
+
+nums = list(map(int, input("Space will separate the number : ").split()))
+f_highest, s_highest = second_large(nums)
+print(f"Highest--{f_highest}\nSecond Highest--{s_highest}")
