76f5d28d07d736f23e1590790536b640c8ee00fc
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-21 23:11:07 +0600

    Added Find most frequnt item with tie

--- count_frequency_with_tie.py
+++ count_frequency_with_tie.py
@@ -2,7 +2,7 @@ def count_frequency_with_tie(nums):
     int_list = list(map(int, nums))
 
     freq = {}
-    high_freq = {}
+    most_freq = {}
     high = None
 
 
@@ -16,14 +16,27 @@ def count_frequency_with_tie(nums):
 
     for key, val in freq.values():
         if high == None or val > high:
-            high_freq.clear()
-            high_freq.update({key, val})
+            most_freq.clear()
+            most_freq.update({key, val})
+
+    ans = most_freq
+
+    tie = {}
+    for key, val in freq.values:
+        if val == most_freq.values():
+            tie.update({key : val})
 
     
+    if tie:
+        lowest = None
+        for key, val in tie.values():
+            if lowest == None or key < lowest:
+                freq.clear()
+                freq[key] = val
 
-
-    return freq
-
+        ans = freq
+        
+    return ans
 
 
 
