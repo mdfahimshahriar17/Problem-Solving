639251feb8bb4d2470dfc062b08af53cfceb0fa8
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-22 11:09:59 +0600

    Keep saved the wrong approch in comment

--- count_frequency_with_tie.py
+++ count_frequency_with_tie.py
@@ -1,47 +1,48 @@
-def count_frequency_with_tie(nums):
-    int_list = list(map(int, nums))
+# def count_frequency_with_tie(nums):
+#     int_list = list(map(int, nums))
 
-    freq = {}
-    most_freq = {}
-    high = None
-
-
-    for num in int_list:
-        if num not in freq:
-            freq[num] = 1
-
-        else:
-            freq[num] += 1
+#     freq = {}
+#     most_freq = {}
+#     high = None
 
 
-    for key, val in freq.items():
-        if high == None or val > high:
-            most_freq.clear()
-            most_freq.update({key, val})
+#     for num in int_list:
+#         if num not in freq:
+#             freq[num] = 1
 
-    ans = most_freq
+#         else:
+#             freq[num] += 1
 
-    tie = {}
-    for key, val in freq.items():
-        if val == most_freq.values():
-            tie.update({key : val})
+
+#     for key, val in freq.items():
+#         if high == None or val > high:
+#             most_freq.clear()
+#             most_freq.update({key, val})
+
+#     ans = most_freq
+
+#     tie = {}
+#     for key, val in freq.items():
+#         if val == most_freq.values():
+#             tie.update({key : val})
 
     
-    if tie:
-        lowest = None
-        for key, val in tie.items():
-            if lowest == None or key < lowest:
-                freq.clear()
-                freq[key] = val
+#     if tie:
+#         lowest = None
+#         for key, val in tie.items():
+#             if lowest == None or key < lowest:
+#                 freq.clear()
+#                 freq[key] = val
 
-        ans = freq
+#         ans = freq
         
-    return ans
+#     return ans
 
 
 
 
 
-nums = input("Space will seperate the numbers\nEnter Numbers : ").split()
-result = count_frequency_with_tie(nums)
-print(result)
+# nums = input("Space will seperate the numbers\nEnter Numbers : ").split()
+# result = count_frequency_with_tie(nums)
+# print(result)
+
