3b015f06dcec05a1cb9dc77eed60e77108fef4ea
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-30 21:50:21 +0600

    Tried another day to count positive integer and added  input system using map

--- count_positive_integer_in_list.py
+++ count_positive_integer_in_list.py
@@ -10,3 +10,20 @@ def count_positive_integer(nums):
 nums = input("Input numbers : ").split()
 result = count_positive_integer(nums)
 print(result)
+
+
+
+
+
+def count_positive_integer(nums):
+    count = 0
+
+    for n in nums:
+        if n > 0:
+            count += 1
+
+    return count
+
+n = list(map(int, input().split()))
+result = count_positive_integer(nums=n)
+print(result)
