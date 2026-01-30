a45f9732eadc62a31b310b806a8fdee57ad09641
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-30 21:54:41 +0600

    Tried different algorithm to count frq in text

--- character_frequency_in_text.py
+++ character_frequency_in_text.py
@@ -16,3 +16,25 @@ text = "python is easy and python is powerful"
 result = character_frequency(text)
 
 print(result)
+
+
+
+
+
+def frequency_of_characters(text):
+    freq = {}
+
+    for cha in text:
+        if cha not in freq:
+            freq[cha] = 1
+
+        else:
+            freq[cha] += 1
+
+    return freq
+#input text without space
+text = input().lower()
+result = frequency_of_characters(text=text)
+if result:
+    for key, val in result.items():
+        print(f"{key} {val}")
