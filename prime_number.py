f3a1c8ee481d30df3fde87cc2d42a0b70698ea71
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-02-02 19:23:28 +0600

    Tried different to solve prime

--- prime_number.py
+++ prime_number.py
@@ -69,3 +69,17 @@ else:
 
 
 
+import math
+def task2():
+    n = int(input())
+    if n <= 1:
+        print("No")
+        return
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            print("No")
+            return
+    print("Yes")
+
+
+task2()
